274d7e2ea5bed1e674ed1a37970498e50685741f
u-nation <Endooooooo7@gmail.com>
2017-12-02 23:02:24 +0900

    refactor pass code logic

--- app/src/main/java/com/example/u_nation/passcodelocksample/MyApplication.java
+++ app/src/main/java/com/example/u_nation/passcodelocksample/MyApplication.java
@@ -2,14 +2,10 @@ package com.example.u_nation.passcodelocksample;
 
 import android.app.Activity;
 import android.app.Application;
-import android.content.res.Configuration;
+import android.content.ComponentCallbacks2;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 
 import com.example.u_nation.passcodelocksample.util.PrefUtil;
-
-import java.util.HashSet;
 
 import timber.log.Timber;
 
@@ -17,94 +13,51 @@ import static com.example.u_nation.passcodelocksample.Constants.PREF_KEY_IS_LOCK
 
 public class MyApplication extends Application implements Application.ActivityLifecycleCallbacks {
 
-    private HashSet<Integer> activityStack = new HashSet<>();
-    private static MyApplication app;
+  private boolean isNeedPassCodeConfirmation = true;
+  private static MyApplication app;
 
-    private Handler handler = new Handler(Looper.getMainLooper());
-    private int orientation = Configuration.ORIENTATION_PORTRAIT;
-    private boolean is_rotated = false;
+  public static MyApplication getInstance() {
+    if (app == null) app = new MyApplication();
+    return app;
+  }
 
-    public static MyApplication getInstance() {
-        if (app == null) app = new MyApplication();
-        return app;
+  @Override public void onCreate() {
+    super.onCreate();
+    Timber.plant(new Timber.DebugTree());
+    PrefUtil.setSharedPreferences(getApplicationContext());
+    registerActivityLifecycleCallbacks(this);
+  }
+
+  @Override public void onTrimMemory(int level) {
+    super.onTrimMemory(level);
+    if (level == ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN) {
+      isNeedPassCodeConfirmation = true;
     }
+  }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Timber.plant(new Timber.DebugTree());
-        PrefUtil.setSharedPreferences(getApplicationContext());
-        registerActivityLifecycleCallbacks(this);
+  @Override public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+  }
+
+  @Override public void onActivityStarted(Activity activity) {
+    if (isNeedPassCodeConfirmation && PrefUtil.getBoolean(PREF_KEY_IS_LOCKED)) {
+      activity.startActivity(PassCodeConfirmationActivity.createIntent(getApplicationContext()));
     }
+    isNeedPassCodeConfirmation = false;
+  }
 
-    @Override
-    public void onTerminate() {
-        unregisterActivityLifecycleCallbacks(this);
-        super.onTerminate();
-    }
+  @Override public void onActivityResumed(Activity activity) {
+  }
 
-    @Override
-    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-        handler.removeCallbacksAndMessages(null);
-    }
+  @Override public void onActivityPaused(Activity activity) {
 
-    @Override
-    public void onActivityStarted(Activity activity) {
-        handler.removeCallbacksAndMessages(null);
-        Timber.i("Started\nactivityStack.size() = %s\nactivity_name = %s", activityStack.size(), activity.toString());
-        boolean isForeground = activityStack.size() == 0 && !is_rotated;
-        is_rotated = false;
-        activityStack.add(activity.hashCode());
-        if (isForeground && PrefUtil.getBoolean(PREF_KEY_IS_LOCKED)) {
-            activity.startActivity(PassCodeConfirmActivity.createIntent(getApplicationContext()));
-        }
-    }
+  }
 
-    @Override
-    public void onActivityResumed(Activity activity) {
+  @Override public void onActivityStopped(Activity activity) {
+  }
 
-    }
+  @Override public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+  }
 
-    @Override
-    public void onActivityPaused(Activity activity) {
-
-    }
-
-    @Override
-    public void onActivityStopped(Activity activity) {
-        activityStack.remove(activity.hashCode());
-        Timber.i("Stopped\nactivityStack.size() = %s\nactivity_name = %s", activityStack.size(), activity.toString());
-
-        // Check Orientation
-        Configuration conf = getResources().getConfiguration();
-        is_rotated = orientation != conf.orientation;
-        orientation = conf.orientation;
-        Timber.i(orientation == Configuration.ORIENTATION_PORTRAIT ? "PORTRAIT" : "LANDSCAPE");
-
-        boolean isBackground = activityStack.size() == 0 && !is_rotated;
-        if (isBackground) {
-            Timber.i("normal isBackground");
-            //doing something
-        }
-
-        if (activityStack.size() == 0 && is_rotated) {
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    is_rotated = false;
-                    Timber.i("postDelayed isBackground");
-                    //doing something
-                }
-            }, 200);
-        }
-    }
-
-    @Override
-    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-
-    }
-
-    @Override
-    public void onActivityDestroyed(Activity activity) {
-    }
+  @Override public void onActivityDestroyed(Activity activity) {
+  }
 }
--- app/src/main/java/com/example/u_nation/passcodelocksample/PassCodeSetActivity.java
+++ app/src/main/java/com/example/u_nation/passcodelocksample/PassCodeSetActivity.java
@@ -22,136 +22,140 @@ import timber.log.Timber;
 
 public class PassCodeSetActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private final String TEXT_MAIN_CONFIRM = "Confirm passcode";
-    private final String TEXT_SUB_CONFIRM = "Please re-enter passcode";
-    private final String TEXT_MAIN_MISTAKE = "Passcode Lock";
-    private final String TEXT_SUB_MISTAKE = "Wrong passcode.Please try it again";
-    private TextView text_main_pass;
-    private TextView text_sub_pass;
-    private ImageView[] array_image_view;
-    private StringBuilder stringBuilder = new StringBuilder();
-    private SparseArray<String> array_box = new SparseArray<>();
-    private Bitmap bitmapBlack = Bitmap.createBitmap(300, 300, Bitmap.Config.ARGB_8888);
-    private Bitmap bitmapGrey = Bitmap.createBitmap(300, 300, Bitmap.Config.ARGB_8888);
-    private int password;
-    private boolean isDoubleCheck = false;
+  private final String TEXT_MAIN_CONFIRM = "Confirm passcode";
+  private final String TEXT_SUB_CONFIRM = "Please re-enter passcode";
+  private final String TEXT_MAIN_MISTAKE = "Passcode Lock";
+  private final String TEXT_SUB_MISTAKE = "Wrong passcode.Please try it again";
+  private TextView text_main_pass;
+  private TextView text_sub_pass;
+  private ImageView[] array_image_view;
+  private StringBuilder stringBuilder = new StringBuilder();
+  private SparseArray<String> array_box = new SparseArray<>();
+  private Bitmap bitmapBlack = Bitmap.createBitmap(300, 300, Bitmap.Config.ARGB_8888);
+  private Bitmap bitmapGrey = Bitmap.createBitmap(300, 300, Bitmap.Config.ARGB_8888);
+  private int password;
+  private boolean isDoubleCheck = false;
 
-    public static Intent createIntent(Context context) {
-        return new Intent(context, PassCodeSetActivity.class);
+  public static Intent createIntent(Context context) {
+    return new Intent(context, PassCodeSetActivity.class);
+  }
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_input_pass_code);
+    initViews();
+    initCircleCanvas();
+  }
+
+  private void initViews() {
+    text_main_pass = findViewById(R.id.text_main_pass);
+    text_sub_pass = findViewById(R.id.text_sub_pass);
+    array_image_view = new ImageView[] {
+        findViewById(R.id.circle1), findViewById(R.id.circle2), findViewById(R.id.circle3),
+        findViewById(R.id.circle4)
+    };
+
+    int[] array_id = {
+        R.id.box0, R.id.box1, R.id.box2, R.id.box3, R.id.box4, R.id.box5, R.id.box6, R.id.box7,
+        R.id.box8, R.id.box9
+    };
+    for (int i = 0; i <= 9; i++) {
+      findViewById(array_id[i]).setOnClickListener(this);
+      array_box.put(array_id[i], String.valueOf(i));
     }
+  }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_input_pass_code);
-        initViews();
-        initCircleCanvas();
-    }
+  private void initCircleCanvas() {
+    Canvas canvas;
+    canvas = new Canvas(bitmapBlack);
+    Paint paint;
+    paint = new Paint();
+    paint.setColor(Color.BLACK);
+    paint.setStyle(Paint.Style.FILL);
+    paint.setAntiAlias(true);
+    canvas.drawCircle(150, 150, 148, paint);
 
-    private void initViews() {
-        text_main_pass = (TextView) findViewById(R.id.text_main_pass);
-        text_sub_pass = (TextView) findViewById(R.id.text_sub_pass);
-        array_image_view = new ImageView[]{(ImageView) findViewById(R.id.circle1), (ImageView) findViewById(R.id.circle2), (ImageView) findViewById(R.id.circle3), (ImageView) findViewById(R.id.circle4)};
+    Canvas canvas2;
+    canvas2 = new Canvas(bitmapGrey);
+    Paint paint2;
+    paint2 = new Paint();
+    paint2.setColor(Color.parseColor("#f5f5f5"));
+    paint2.setStyle(Paint.Style.FILL);
+    paint2.setAntiAlias(true);
+    canvas2.drawCircle(150, 150, 148, paint2);
+  }
 
-        int[] array_id = {R.id.box0, R.id.box1, R.id.box2, R.id.box3, R.id.box4, R.id.box5, R.id.box6, R.id.box7, R.id.box8, R.id.box9};
-        for (int i = 0; i <= 9; i++) {
-            findViewById(array_id[i]).setOnClickListener(this);
-            array_box.put(array_id[i], String.valueOf(i));
+  private void initStringBuilder() {
+    stringBuilder.setLength(0);
+    stringBuilder.trimToSize();
+  }
+
+  private void initCircleColor() {
+    for (ImageView circle : array_image_view) circle.setImageBitmap(bitmapGrey);
+  }
+
+  @Override public void onClick(View v) {
+    if (array_box.indexOfKey(v.getId()) >= 0) inputPassword(array_box.get(v.getId()));
+  }
+
+  public void onDelete(View view) {
+    int length = stringBuilder.length();
+    deleteCircleColor(length);
+    if (length != 0) stringBuilder.deleteCharAt(length - 1);
+  }
+
+  private void deleteCircleColor(int length) {
+    if (length > 0) array_image_view[length - 1].setImageBitmap(bitmapGrey);
+  }
+
+  private void inputPassword(String password) {
+    Timber.d(password);
+    int length = stringBuilder.length();
+    if (length > 3) return;
+    array_image_view[length].setImageBitmap(bitmapBlack);
+    stringBuilder.append(password);
+    if (length == 3) {
+      new Handler().postDelayed(new Runnable() {
+        @Override public void run() {
+          confirmPassword();
         }
+      }, 200);
     }
+  }
 
-    private void initCircleCanvas() {
-        Canvas canvas;
-        canvas = new Canvas(bitmapBlack);
-        Paint paint;
-        paint = new Paint();
-        paint.setColor(Color.BLACK);
-        paint.setStyle(Paint.Style.FILL);
-        paint.setAntiAlias(true);
-        canvas.drawCircle(150, 150, 148, paint);
-
-        Canvas canvas2;
-        canvas2 = new Canvas(bitmapGrey);
-        Paint paint2;
-        paint2 = new Paint();
-        paint2.setColor(Color.parseColor("#f5f5f5"));
-        paint2.setStyle(Paint.Style.FILL);
-        paint2.setAntiAlias(true);
-        canvas2.drawCircle(150, 150, 148, paint2);
-    }
-
-    private void initStringBuilder() {
-        stringBuilder.setLength(0);
-        stringBuilder.trimToSize();
-    }
-
-    private void initCircleColor() {
-        for (ImageView circle : array_image_view) circle.setImageBitmap(bitmapGrey);
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (array_box.indexOfKey(v.getId()) >= 0) inputPassword(array_box.get(v.getId()));
-    }
-
-    public void onDelete(View view) {
-        int length = stringBuilder.length();
-        deleteCircleColor(length);
-        if (length != 0) stringBuilder.deleteCharAt(length - 1);
-    }
-
-    private void deleteCircleColor(int length) {
-        if (length > 0) array_image_view[length - 1].setImageBitmap(bitmapGrey);
-    }
-
-    private void inputPassword(String password) {
-        Timber.d(password);
-        int length = stringBuilder.length();
-        if (length > 3) return;
-        array_image_view[length].setImageBitmap(bitmapBlack);
-        stringBuilder.append(password);
-        if (length == 3) new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                confirmPassword();
-            }
-        }, 200);
-    }
-
-    private void confirmPassword() {
-        if (isDoubleCheck) {
-            if (password == Integer.parseInt(stringBuilder.toString())) {
-                ShowToast.show("Locked!", this);
-                PrefUtil.putInt(Constants.PREF_KEY_PASSWORD, password);
-                PrefUtil.putBoolean(Constants.PREF_KEY_IS_LOCKED, true);
-                finish();
-            } else {
-                text_main_pass.setText(TEXT_MAIN_MISTAKE);
-                text_sub_pass.setText(TEXT_SUB_MISTAKE);
-                isDoubleCheck = false;
-                initStringBuilder();
-                initCircleColor();
-            }
-        } else {
-            isDoubleCheck = true;
-            text_main_pass.setText(TEXT_MAIN_CONFIRM);
-            text_sub_pass.setText(TEXT_SUB_CONFIRM);
-            password = Integer.parseInt(stringBuilder.toString());
-            initStringBuilder();
-            initCircleColor();
-        }
-    }
-
-    public void onCancel(View view) {
+  private void confirmPassword() {
+    if (isDoubleCheck) {
+      if (password == Integer.parseInt(stringBuilder.toString())) {
+        ShowToast.show("Locked!", this);
+        PrefUtil.putInt(Constants.PREF_KEY_PASSWORD, password);
+        PrefUtil.putBoolean(Constants.PREF_KEY_IS_LOCKED, true);
         finish();
+      } else {
+        text_main_pass.setText(TEXT_MAIN_MISTAKE);
+        text_sub_pass.setText(TEXT_SUB_MISTAKE);
+        isDoubleCheck = false;
+        initStringBuilder();
+        initCircleColor();
+      }
+    } else {
+      isDoubleCheck = true;
+      text_main_pass.setText(TEXT_MAIN_CONFIRM);
+      text_sub_pass.setText(TEXT_SUB_CONFIRM);
+      password = Integer.parseInt(stringBuilder.toString());
+      initStringBuilder();
+      initCircleColor();
     }
+  }
 
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            finish();
-            return true;
-        }
-        return super.onKeyDown(keyCode, event);
+  public void onCancel(View view) {
+    finish();
+  }
+
+  @Override public boolean onKeyDown(int keyCode, KeyEvent event) {
+    if (keyCode == KeyEvent.KEYCODE_BACK) {
+      finish();
+      return true;
     }
+    return super.onKeyDown(keyCode, event);
+  }
 }
